8a9f75f141b4f8bad005d87287e4deda65cb54ed
hoyeonbhb <123072611+hoyeonbhb@users.noreply.github.com>
2025-07-25 19:13:47 +0900

    Update envelope.js

--- envelope.js
+++ envelope.js
@@ -60,7 +60,7 @@ function ghost_exit(){
                 fill: "forwards"
             };
             ghost.animate(keyframes, options);          
-        }, 5000);
+        }, 1000);
         keyframes = [
             {opacity: 0},
             {opacity: 0},
@@ -71,7 +71,7 @@ function ghost_exit(){
         ];
         options = {
             delay: 0,
-            duration: 5000,
+            duration: 1000,
             easing: "ease-in",
             easing: "ease-out",
             iterations: 1,
